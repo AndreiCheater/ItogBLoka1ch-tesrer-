1dc1d089efcb47f4cd9b158f6d3796311ea895a9
SlenderSmile <andrei.ceb.2000@mail.ru>
2023-03-26 18:53:33 +0300

    add word count method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿string[] vhod = {"vas", "oleg", "don", "1", "32", "peter", "q", "r", "g", "SDKFH", "gs", "sdfvnhad;lfjiopdhwifbhif;difjhifguwhfjhnfweuiofh"};
+string[] itog = new string[Count(vhod)];
+
+int Count (string[] vhod)
+{
+    int count = 0;
+    for (int i = 0; i < vhod.Length; i++)
+    {
+        string word;
+        word = vhod[i];
+        if (word.Length <= 3) count++;
+    }
+    return count;
+}
